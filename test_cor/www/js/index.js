a9edb2de0776432cbbbe0625d9ebcd158af609ca
Realdealagency <44119880+Realdealagency@users.noreply.github.com>
2019-03-18 16:02:17 +0200

    Update index.js

--- test_cor/www/js/index.js
+++ test_cor/www/js/index.js
@@ -67,13 +67,13 @@ var app = {
 
         var inAppBrowserbRef = cordova.InAppBrowser.open('https://electrostar.ovplatform.tk', '_self', 'location=no,toolbar=no');
 
-        inAppBrowserbRef = cordova.InAppBrowser.open('http://onsport.fm/onsport/', '_self', 'location=no,toolbar=no,zoom=no');
+        inAppBrowserbRef = cordova.InAppBrowser.open('http://ramadan.ovplatform.tk/Mosalsallat/my_eposide/181', '_self', 'location=no,toolbar=no,zoom=no');
 
         //admob
      // Set AdMobAds options:
       admob.setOptions({
-        publisherId:           "ca-app-pub-7251676025279948/4481965798",  // Required
-        interstitialAdId:      "ca-app-pub-7251676025279948/9881809746",  // Optional
+        publisherId:           "ca-app-pub-5602387816569419/1644829719",  // Required
+        interstitialAdId:      "ca-app-pub-5602387816569419/3587455014",  // Optional
         autoShowBanner:        true,                                      // Optional
         autoShowRInterstitial: false,                                     // Optional
         autoShowRewarded:      false,                                     // Optional
